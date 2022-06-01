80b395f540af9a5305f7faf05eda8c8967235542
Nadim <nahlenadim@gmail.com>
2022-06-01 22:05:35 +0300

    fixing edit action

--- main.js
+++ main.js
@@ -18,13 +18,32 @@ $(window).on("load",function(){
        }
    }); 
     
-   $(document).on('click', '.delete', function(){
-    $(this).parent().parent().fadeOut('slow', function(){
-        $(this).remove();
-        localStorage.setItem("tasks", $('#tasks').html());
+   //the delete action
+    $(document).on('click', '.delete', function(){
+        $(this).parent().parent().fadeOut('slow', function(){
+            $(this).remove();
+            localStorage.setItem("tasks", $('#tasks').html());
+        });
     });
-});
-
+    const edit = document.querySelector(".edit");
+    $('.edit').click(function(event){
+        if( edit.innerText == "Edit" ){
+            
+            $('.title-text').attr('readonly', false);
+            $('.description').attr('readonly', false);
+            $('.edit').html("save");
+            localStorage.setItem("tasks", $('#tasks').html());
+            
+        }
+        else {
+            $('.edit').html("Edit");
+            $('.title-text').attr('readonly', true);
+            $('.description').attr('readonly', true);
+            localStorage.setItem("tasks", $('#tasks').html());
+        }
+        
+        localStorage.setItem("tasks", $('#tasks').html());
+    })
 
 })   
     
