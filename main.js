971bee2941c2d5b85cfc8128760b3b5bfd728deb
Nadim <nahlenadim@gmail.com>
2022-06-01 15:55:21 +0300

    fixing the delete button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(window).on("load",function(){
    
-    
+    $('#tasks').html(localStorage.getItem("tasks"));
     
     $('#form').submit(function(event){
        event.preventDefault();
@@ -9,134 +9,21 @@ $(document).ready(function(){
        var descriptionList = $('#description').val();
        
        if (titleList != ''){
-            $("#tasks").append('<div class="task"> <div class="content"><input type="text" class="title-text" value='+titleList+'  readonly><input type="text" class="description" value='+descriptionList+'  readonly> <div class="actions"><button class="edit">Edit</button><button class="delete">Delete</button></div></div>');
+            $("#tasks").append('<div class="task"> <div class="content"><input type="text" class="title-text" value='+titleList+'  readonly><input type="text" class="description" value='+descriptionList+'  readonly> <div class="actions"><button class="edit" value="0">Edit</button><button class="delete">Delete</button></div></div>');
 
-           localStorage.setItem("listItems", $('#tasks').html());
+           localStorage.setItem("tasks", $('#tasks').html());
 
            $('#title').val("");
            $('#description').val('');
        }
    }); 
     
-    
-    
- /* const formTitle = title.value;
-    const formDescription = description.value;
-    
-    if(!formTitle){
-        alert("please fill out the title");
-        return;
-    }
-    else{
-       
-        $('.title-text').val(formTitle);
-        $('.description').val(formDescription);
-    }
-        
-        const edit = document.querySelector(".edit");
-        const inputTitle = document.querySelector(".title-text");
-        const inputDescription = document.querySelector(".description");
-        const deleteTask = document.querySelector(".delete");
-        edit.addEventListener('click', (e) => {
-			if (edit.innerText.toLowerCase() == "edit") {
-				edit.innerText = "Save";
-				inputTitle.removeAttribute("readonly");
-				inputTitle.focus();
-                inputDescription.removeAttribute("readonly");
-			} else {
-				edit.innerText = "Edit";
-				inputTitle.setAttribute("readonly", "readonly");
-				inputDescription.setAttribute("readonly", "readonly");
-			}
-		});
-        $(".task").on('click', '.delete', function() {
-			$(".task").fadeOut(200);
-        });
- 
-   
+   $(document).on('click', '.delete', function(){
+    $(this).parent().parent().fadeOut('slow', function(){
+        $(this).remove();
+    });
 });
-})
-*/
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   /* $(window).on("load",function(){
-        var count =0;
-        $(".addtask").click(function() {
-            var titleList = $("#title").val();
-            var descriptionList = $('#description').val();
-            
-            
-            if (titleList != ''){
-                $("#tasks").append('<div class="task"> <div class="content"><input type="text" class="title-text" value='+titleList+' readonly><input type="text" class="description" value='+descriptionList+' readonly> <div class="actions"><button class="edit">Edit</button><button class="delete">Delete</button></div></div>');
-                
-            }
-            $('#title').val('');
-            $('#description').val('');
-            })
-        $(document).on('click', '.delete', function() {
-            $(".content").remove();
-        });
-    })
+
+
+})   
     
-
-    /* const formTitle = title.value;
-    const formDescription = description.value;
-    
-    if(!formTitle){
-        alert("please fill out the title");
-        return;
-    }
-    else{
-       
-        $('.title-text').val(formTitle);
-        $('.description').val(formDescription);
-    }
-
-        
-        const edit = document.querySelector(".edit");
-        const inputTitle = document.querySelector(".title-text");
-        const inputDescription = document.querySelector(".description");
-        const deleteTask = document.querySelector(".delete");
-
-        edit.addEventListener('click', (e) => {
-			if (edit.innerText.toLowerCase() == "edit") {
-				edit.innerText = "Save";
-				inputTitle.removeAttribute("readonly");
-				inputTitle.focus();
-                inputDescription.removeAttribute("readonly");
-			} else {
-				edit.innerText = "Edit";
-				inputTitle.setAttribute("readonly", "readonly");
-				inputDescription.setAttribute("readonly", "readonly");
-			}
-		});
-
-        $(".task").on('click', '.delete', function() {
-			$(".task").fadeOut(200);
-        });
- 
-   
-
-});
-})
-*/
