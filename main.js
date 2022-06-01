9b2067b6445cfaf1873e457d07149a0c0ba183f7
Nadim <nahlenadim@gmail.com>
2022-06-01 15:40:50 +0300

    adding local storage

--- main.js
+++ main.js
@@ -1,9 +1,91 @@
+$(document).ready(function(){
+   
+    
+    
+    $('#form').submit(function(event){
+       event.preventDefault();
+       
+       var titleList = $('#title').val();
+       var descriptionList = $('#description').val();
+       
+       if (titleList != ''){
+            $("#tasks").append('<div class="task"> <div class="content"><input type="text" class="title-text" value='+titleList+'  readonly><input type="text" class="description" value='+descriptionList+'  readonly> <div class="actions"><button class="edit">Edit</button><button class="delete">Delete</button></div></div>');
 
-    $(window).on("load",function(){
+           localStorage.setItem("listItems", $('#tasks').html());
+
+           $('#title').val("");
+           $('#description').val('');
+       }
+   }); 
+    
+    
+    
+ /* const formTitle = title.value;
+    const formDescription = description.value;
+    
+    if(!formTitle){
+        alert("please fill out the title");
+        return;
+    }
+    else{
+       
+        $('.title-text').val(formTitle);
+        $('.description').val(formDescription);
+    }
+        
+        const edit = document.querySelector(".edit");
+        const inputTitle = document.querySelector(".title-text");
+        const inputDescription = document.querySelector(".description");
+        const deleteTask = document.querySelector(".delete");
+        edit.addEventListener('click', (e) => {
+			if (edit.innerText.toLowerCase() == "edit") {
+				edit.innerText = "Save";
+				inputTitle.removeAttribute("readonly");
+				inputTitle.focus();
+                inputDescription.removeAttribute("readonly");
+			} else {
+				edit.innerText = "Edit";
+				inputTitle.setAttribute("readonly", "readonly");
+				inputDescription.setAttribute("readonly", "readonly");
+			}
+		});
+        $(".task").on('click', '.delete', function() {
+			$(".task").fadeOut(200);
+        });
+ 
+   
+});
+})
+*/
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   /* $(window).on("load",function(){
         var count =0;
         $(".addtask").click(function() {
             var titleList = $("#title").val();
             var descriptionList = $('#description').val();
+            
+            
             if (titleList != ''){
                 $("#tasks").append('<div class="task"> <div class="content"><input type="text" class="title-text" value='+titleList+' readonly><input type="text" class="description" value='+descriptionList+' readonly> <div class="actions"><button class="edit">Edit</button><button class="delete">Delete</button></div></div>');
                 
@@ -12,7 +94,7 @@
             $('#description').val('');
             })
         $(document).on('click', '.delete', function() {
-                $(".task").remove();
+            $(".content").remove();
         });
     })
     
